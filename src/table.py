b3abdb132b8ca5204a7b63342bfe62ed5c89d180
Your Name <crispamares@gmail.com>
2012-09-28 15:58:49 -0400

    * Some tests with blocking methods with metaprograming.

--- src/table.py
+++ src/table.py
@@ -14,7 +14,44 @@ class Table(object):
 
     def col(self, name):
         return self._df[name].tolist()
-    
+
+
+class RestrictedMethod(object):
+    def __get__(self, obj, objtype):
+        raise AttributeError("Access denied.")
+
+class RestrictingMetaWrapper(type):
+    def __new__(cls, name, bases, dic):
+        block = dic.get('_block', [])
+
+        for attr in block:
+            dic[attr] = RestrictedMethod()
+
+        return type.__new__(cls, name, bases, dic) # note we inject the base class here
+
+
+
+def restrict_methods(*args):
+    def wrap(cls):
+        for attr in args:
+            setattr(cls, attr, RestrictedMethod())
+        return cls
+    return wrap
+
+
+#class RestrictingMetaWrapper(type):
+#    def __new__(cls, name, bases, dic):
+#        wrapped = dic['_w']
+#        block = dic.get('_block', [])
+#
+#        new_class_dict = {}
+#        new_class_dict.update(wrapped.__dict__)
+#        for attr_to_block in block:
+#            del new_class_dict[attr_to_block]
+#        new_class_dict.update(dic)
+#        
+#        return type.__new__(cls, name, bases, new_class_dict)
+#
 class RestrictingWrapper(object):
     def __init__(self, w, block):
         self._w = w
@@ -28,8 +65,14 @@ class RestrictingWrapper(object):
 class Row2(RestrictingWrapper):
     def __init__(self, *args, **kargs):
         w = pandas.Series(*args, **kargs)
-        block = []
+        block = ['index']
         RestrictingWrapper.__init__(self, w, block)
+
+class Row3(pandas.Series):
+    __metaclass__ = RestrictingMetaWrapper
+    _block = ['max']
+    
+        
 
 class Rows(list):
     pass
