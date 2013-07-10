37dd30a5e2f82a84fadb6da5065127caf5522614
Juan Morales <juan.morales@upm.es>
2013-07-10 14:32:58 +0200

    renamed dynfilter to dymselect

--- src/dynamics/dselect.py
+++ src/dynamics/dselect.py
@@ -12,9 +12,9 @@ from epubsub.bus import Bus
 from collections import OrderedDict
 
 
-class DynFilter(IPublisher):
+class DynSelect(IPublisher):
     '''
-    This class maintain the state of the Filter Interactive Dynamic
+    This class maintain the state of the Select Interactive Dynamic
     '''
 
     def __init__(self, name):
@@ -23,37 +23,37 @@ class DynFilter(IPublisher):
         '''
         self._name = name
         self._conditions = OrderedDict()
-        self._computed_sieve = None
+        self._computed_reference = None
         
         topics = ['change', 'remove']
         bus = Bus(prefix= 'f.'+self._name+'.')
         IPublisher.__init__(self, bus, topics)
         
-    def add_condition(self, sieve, query = None, name=None):
+    def add_condition(self, reference, query = None, name=None):
         ''' 
-        @param sieve: Sieve is mandatory
+        @param reference: Reference is mandatory
         @param query: If the condition is explicit then a query is needed.
           Providing an explicit condition is useful for recomputing the 
-          sieve if the dataset changes 
+          reference if the dataset changes 
         @param name: If not provided a uuid is generated 
         '''
         name = name if name is None else str(uuid.uuid4())
         if self._conditions.has_key(name):
             raise ValueError("Already exists a condition with the name given")
-        self._conditions[name] = dict(sieve=sieve, query=query)
+        self._conditions[name] = dict(reference=reference, query=query)
         self._dirty()
         
         self._bus.publish('change', name)
         
-    def set_condition(self, name, sieve, query=None):
+    def set_condition(self, name, reference, query=None):
         ''' 
         @param name: The key of the condition. 
-        @param sieve: Sieve is mandatory
+        @param reference: Reference is mandatory
         @param query: If the condition is explicit then a query is needed.
           Providing an explicit condition is useful for recomputing the 
-          sieve if the dataset changes 
+          reference if the dataset changes 
         '''
-        self._conditions[name] = dict(sieve=sieve, query=query)
+        self._conditions[name] = dict(reference=reference, query=query)
         self._dirty()
         
         self._bus.publish('change', name)
@@ -61,10 +61,10 @@ class DynFilter(IPublisher):
     def remove_condition(self, name):
         ''' 
         @param name: The key of the condition. 
-        @param sieve: Sieve is mandatory
+        @param reference: Reference is mandatory
         @param query: If the condition is explicit then a query is needed.
           Providing an explicit condition is useful for recomputing the 
-          sieve if the dataset changes 
+          reference if the dataset changes 
         '''
         if self._conditions.has_key(name):
             raise ValueError("There is no condition with the given name")
@@ -77,32 +77,32 @@ class DynFilter(IPublisher):
         return len(self._conditions) == 0
     
     @property
-    def sieve(self):
-        '''The sieve resulting of the accumulation of every condition.
-        A sieve is a set of indices or None if there are no conditions'''
-        if self._computed_sieve is None:
-            self._computed_sieve = self._compute_sieve()
-        return self._computed_sieve
+    def ref(self):
+        '''The reference resulting of the accumulation of every condition.
+        A reference is a set of indices or None if there are no conditions'''
+        if self._computed_reference is None:
+            self._computed_reference = self._compute_reference()
+        return self._computed_reference
 
     def query(self, index):
         '''The query resulting of the accumulation of every condition.
         @param index: A string or list of strings'''
         if self.is_empty():
             return {}
-        if self._computed_sieve is None:
-            self._computed_sieve = self._compute_sieve()
+        if self._computed_reference is None:
+            self._computed_reference = self._compute_reference()
         if isinstance(index, list):
             NotImplementedError('Multi Index is not supported yet')
-        return { index : {'$in': list(self._computed_sieve)} }
+        return { index : {'$in': list(self._computed_reference)} }
         
     def _dirty(self):
-        self._computed_sieve = None
+        self._computed_reference = None
         
-    def _compute_sieve(self):
+    def _compute_reference(self):
         if self.is_empty():
             return None
-        sieve = set()
+        reference = set()
         for c in self._conditions.values():
-            sieve = sieve.union(c['sieve'])
-        return sieve
+            reference = reference.union(c['reference'])
+        return reference
     
