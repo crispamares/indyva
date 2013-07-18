d8bdbcb2f58972edc117c5d3a0c17531f2caa4c1
Juan Morales <juan.morales@upm.es>
2013-07-18 18:48:19 +0200

    json serialization

--- src/facade/endpoint.py
+++ src/facade/endpoint.py
@@ -7,6 +7,7 @@ Created on 17/07/2013
 import zmq
 import front
 from loop import when_message
+import json
 
 class Endpoint(object):
     '''
@@ -24,19 +25,31 @@ class Endpoint(object):
         
         self.socket = self.ctx.socket(zmq.REP)
         self.socket.bind('tcp://127.0.0.1:10111')
-        
+        #self.socket.bind('ipc:///tmp/1')
         self._stopped = True
         
     @when_message
     def recive(self):
         event = self.socket.poll(1)
         if event:
-            msg = self.socket.recv()
+            json_msg = self.socket.recv()
+            msg = json.loads(json_msg)
             response = self.front.call(msg['content'])
-            self.socket.send(response)
+            json_response = json.dumps(response)
+            self.socket.send(json_response)
         if not self._stopped:
             self.recive()
     
+    def fast_recive(self):
+        while True:
+            json_msg = self.socket.recv()
+            msg = json.loads(json_msg)
+            response = self.front.call(msg['content'])
+            json_response = json.dumps(response)
+            self.socket.send(json_response)
+            if self._stopped:
+                break
+            
     def run(self):
         self._stopped = False
         self.recive()
