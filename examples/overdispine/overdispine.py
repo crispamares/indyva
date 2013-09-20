7fa611bcac9cac1903a5e6e5dae0841bb66f04cc
Juan Morales del Olmo <juan.morales@upm.es>
2013-09-20 18:19:18 +0200

    size, length and angles histograms

--- examples/overdispine/overdispine.py
+++ examples/overdispine/overdispine.py
@@ -9,7 +9,7 @@ from collections import OrderedDict
 
 from PyQt4 import QtGui, Qt
 
-from row_viz import RowViz
+from row_viz import RowViz, ListView
 import data_adquisition
 
 
@@ -19,52 +19,17 @@ class MainWindow(QtGui.QMainWindow):
     def __init__(self, *args, **kwargs):
         QtGui.QMainWindow.__init__(self, *args, **kwargs)
 
-        self.plots = OrderedDict()
+
 
         self.build_gui()
 
-    def add_plot(self, name, plot):
-        self.plots[name] = plot
-        plot.setObjectName('_plot_'+name)
-
-    def remove_plot(self, name):
-        self.plots.pop(name)
-        
-    def render_plots(self):
-        # insert plots
-        for i, name in enumerate(self.plots):
-            child = self.viz_area.findChild(QtGui.QLabel, name='_plot_'+name)
-            if child is None:
-                self.v_layout.insertWidget(i, self.plots[name])
-        # remove plots
-        children_plots = { str(o.objectName()).replace('_plot_', '') : o 
-                          for o in self.viz_area.children() 
-                          if str(o.objectName()).startswith('_plot_')}
-        plots_to_remove = set(children_plots.keys()).difference(self.plots.keys())
-        
-        for plot in plots_to_remove:
-            self.v_layout.removeWidget(children_plots[plot])
-            children_plots[plot].deleteLater()
-        
-        self.scroll.adjustSize()
         
     def build_gui(self):
         self.resize(1000, 700)
         self.setWindowTitle('OverDispine '+ __version__)
 
-        #self.v_layout.setSizeConstraint(self.v_layout.SetMinAndMaxSize)
-
-        self.scroll = QtGui.QScrollArea(self.centralWidget())
-        
-        self.viz_area = QtGui.QWidget(self.scroll)
-        self.v_layout = QtGui.QVBoxLayout(self.viz_area)
-        #self.v_layout.setSizeConstraint(self.v_layout.SetNoConstraint)
-        self.scroll.setBackgroundRole(Qt.QPalette.Dark)
-        self.scroll.setWidget(self.viz_area)
-        self.scroll.setWidgetResizable(True)
-        self.scroll.setVerticalScrollBarPolicy(2)
-
-        self.setCentralWidget(self.scroll)
+        self.list_view = ListView()
+        self.setCentralWidget(self.list_view.widget)
         
 
 def main():
@@ -73,17 +38,15 @@ def main():
     main_window = MainWindow()
     main_window.show()
 
+    view = main_window.list_view
 
-        
-    main_window.add_plot('lpaco1', QtGui.QLabel('paco label', main_window))
-    main_window.add_plot('lpaco3', QtGui.QLabel('paco label 3', main_window))
-    main_window.add_plot('lpaco2', QtGui.QLabel('paco label 2', main_window))
-    for i in range(10):
-        r = RowViz()
-        main_window.add_plot('paco{}'.format(i), r)
-        r.render()
-    main_window.render_plots()
+    view.add_plot('lpaco1', QtGui.QLabel('paco label', main_window))
+    view.add_plot('lpaco3', QtGui.QLabel('paco label 3', main_window))
+    view.add_plot('lpaco2', QtGui.QLabel('paco label 2', main_window))
 
+    table = data_adquisition.create_table()
+    view.table = table
+    view.update_view()
     
     #main_window.scroll.widget().resize(300, 300)
 
--- examples/overdispine/row_viz.py
+++ examples/overdispine/row_viz.py
@@ -4,9 +4,10 @@ Created on 04/09/2013
 
 @author: jmorales
 '''
-from PyQt4 import Qt
+from PyQt4 import Qt, QtGui
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from collections import OrderedDict
 
 
 
@@ -18,21 +19,110 @@ class RowViz(FigureCanvas):
         '''
         Constructor
         '''
-        FigureCanvas.__init__(self, Figure(figsize=(8,1)))
-        self.setMinimumHeight(120)
+        FigureCanvas.__init__(self, Figure((8,3)))
+        self.setMinimumHeight(220)
         self.setSizePolicy(Qt.QSizePolicy.Preferred, Qt.QSizePolicy.Preferred)
         self.updateGeometry()
 
-        self.ax_1 = self.figure.add_subplot(141)
-        self.ax_2 = self.figure.add_subplot(142)
-        self.ax_3 = self.figure.add_subplot(143)
-        self.ax_4 = self.figure.add_subplot(144)
-        
-    def render(self):
-        self.ax_1.hist([1,1,1,1,3,3,3,4,5,7,7,7,7])
-        self.ax_2.hist([1,1,1,1,3,3,3,4,5,7,7,7,7])
-        self.ax_3.hist([1,1,1,1,3,3,3,4,5,7,7,7,7])
-        self.ax_4.hist([1,1,1,1,3,3,3,4,5,7,7,7,7])
+        self.ax_1 = self.figure.add_subplot(141, title='points')
+        self.ax_2 = self.figure.add_subplot(142, title='size')
+        self.ax_3 = self.figure.add_subplot(143, title='length')
+        self.ax_4 = self.figure.add_subplot(144, title='angle')
 
+        self.spines = None
+        self.dendrite_id = ''
+
+    def mousePressEvent( self, event ):
+        print "Click"
+        
+    def update_view(self):
+        if self.spines is None:
+            raise Exception('No spines assigned before painting')
+        
+        print self.dendrite_id
+        self.figure.suptitle(self.dendrite_id)        
+        
+        try:
+            #self.ax_1.hist([1,1,1,1,3,3,3,4,5,7,7,7,7])
+            self.ax_2.hist(self.spines['size'])
+            self.ax_3.hist(self.spines['length'])
+            self.ax_4.hist(self.spines['angle'])
+        except Exception, e:
+            QtGui.QMessageBox.warning(None, 'Painting ' + self.dendrite_id,
+                                           str(e) )
+        
+        self.figure.tight_layout()
+        
+    def update(self):
+        self.figure.tight_layout()
+        FigureCanvas.update(self)
+
+class ListView(object):
     
+    def __init__(self, table=None):
+        self.table = table
+        self.plots = OrderedDict()
+
+        self.scroll = QtGui.QScrollArea()
+        
+        self.viz_area = QtGui.QWidget(self.scroll)
+        self.v_layout = QtGui.QVBoxLayout(self.viz_area)
+        #self.v_layout.setSizeConstraint(self.v_layout.SetNoConstraint)
+        self.scroll.setBackgroundRole(Qt.QPalette.Dark)
+        self.scroll.setWidget(self.viz_area)
+        self.scroll.setWidgetResizable(True)
+        self.scroll.setVerticalScrollBarPolicy(2)
+    
+    @property
+    def widget(self):
+        return self.scroll
+    
+    def add_plot(self, name, plot):
+        self.plots[name] = plot
+        plot.setObjectName('_plot_'+name)
+
+    def remove_plot(self, name):
+        self.plots.pop(name)
+        
+    def render_plots(self):
+        # insert plots
+        for i, name in enumerate(self.plots):
+            child = self.viz_area.findChild(QtGui.QLabel, name='_plot_'+name)
+            if child is None:
+                plot = self.plots[name]
+                self.v_layout.insertWidget(i, plot)
+                if isinstance(plot, RowViz): plot.update_view()
+        # remove plots
+        children_plots = { str(o.objectName()).replace('_plot_', '') : o 
+                          for o in self.viz_area.children() 
+                          if str(o.objectName()).startswith('_plot_')}
+        plots_to_remove = set(children_plots.keys()).difference(self.plots.keys())
+        
+        for plot in plots_to_remove:
+            self.v_layout.removeWidget(children_plots[plot])
+            children_plots[plot].deleteLater()
+        
+        self.scroll.adjustSize()
+        
+    
+    def update_view(self):
+        if self.table is None:
+            raise Exception('No table assigned before painting')
+            
+        dendrites = sorted(self.table.distinct('dendrite_id'))
+        
+        for dendrite in dendrites:
+            v_spines = self.table.find({'dendrite_id':dendrite},
+                                       {'unroll_pos':True,
+                                        'size':True,
+                                        'length':True,
+                                        'angle':True})
+            spines = v_spines.get_data('c_list')
+            
+            plot = RowViz()
+            plot.spines = spines
+            plot.dendrite_id = dendrite
+            self.add_plot(dendrite, plot)
+        
+        self.render_plots()
     
