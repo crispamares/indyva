ced541650be9132c776bea744fa4a62009420390
Juan Morales <juan.morales@upm.es>
2013-10-15 03:47:02 +0200

    Now the hub unsubscribe deleted objects automatically. --> No memory leaks

--- src/epubsub/hub.py
+++ src/epubsub/hub.py
@@ -5,27 +5,44 @@ Created on 06/06/2013
 @author: jmorales
 '''
 
+from weakref import WeakValueDictionary
+
 class Hub(object):
     '''
     For common uses the Bus is preferred 
     
     The broker of the pubsub system. Handles the subscriptions table.
+    Deleted object's destinations are unsubscribed automatically.  
+    
+    If the destination (a callable) is a method then a weakref of the object 
+    (the method's owner) is saved in order to know when the object is collected 
+    by the gc. 
+    
+    
     '''
 
     def __init__(self):
         self._subscriptions = {}
+        self._subscribers = WeakValueDictionary()
 
     def _subscribe(self, topic, destination, only_once=False, group_id=None):
         ''' TODO: publish new subscriptions and new topics through meta topic'''
-        if not self._subscriptions.has_key(topic):
-            self._subscriptions[topic] = {}
-        topic_destinations = self._subscriptions[topic]
-        
+        oid = None
+        topic_destinations = self._subscriptions.setdefault(topic, {})
+
         subscription_info = {}
         if only_once: subscription_info['only_once'] = True
         if group_id is not None: subscription_info['group_id'] = group_id 
         
-        topic_destinations[destination] = subscription_info
+        subscriber = getattr(destination, 'im_self', None)
+        if subscriber is not None:
+            oid = id(subscriber)
+            self._subscribers[oid] = subscriber
+            subscription_info['subscriber'] = oid 
+            destination = destination.__name__
+            
+        destinations = topic_destinations.setdefault(oid, {})
+        destinations[destination] = subscription_info
               
     def subscribe(self, topic, destination):
         self._subscribe(topic, destination, only_once=False)
@@ -34,7 +51,16 @@ class Hub(object):
         self._subscribe(topic, destination, only_once=True)
         
     def unsubscribe(self, topic, destination):
-        self._subscriptions[topic].pop(destination)
+        subscriber = getattr(destination, 'im_self', None)
+        oid = id(subscriber) if subscriber else None
+        destination = destination.__name__ if subscriber else destination
+        self._unsubscribe(topic, oid, destination)
+            
+    def _unsubscribe(self, topic, oid, destination):
+        if oid is None or oid in self._subscribers:
+            self._subscriptions[topic][oid].pop(destination)
+        else:
+            self._subscriptions[topic].pop(oid)
     
     def close(self, topic):
         '''Removes all the subscriptions to a topic. 
@@ -44,20 +70,37 @@ class Hub(object):
         self._subscriptions.pop(topic, None) 
         
     def _unsubscribe_by_group_id(self, group_id):
-        for topic, destinations in self._subscriptions.items(): 
-            for destination, subscription_info in destinations.items():
-                if subscription_info.get('group_id', None) == group_id:
-                    self.unsubscribe(topic, destination)
+        oids_to_remove = []
+        for topic, oids in self._subscriptions.items(): 
+            for oid, destinations in oids.items():
+                if oid is not None and oid not in self._subscribers:
+                    oids_to_remove.append(oid)
+                    continue
+                for destination, subscription_info in destinations.items():
+                    if subscription_info.get('group_id', None) == group_id:
+                        self._unsubscribe(topic, oid, destination)
+        for oid in oids_to_remove:
+            self._subscriptions[topic].pop(oid)    
+                    
         
     def publish(self, topic, msg):
         ''' TODO: publish No one subscribe through meta topic'''
-        destinations = self._subscriptions.get(topic, {})
-        for destination, subscription_info in destinations.items():
-            self._send_msg(topic, destination, msg)
-            if subscription_info.get('only_once', False):
-                self.unsubscribe(topic, destination)
+        oids_to_remove = []
+        oids = self._subscriptions.get(topic, {})
+        for oid, destinations in oids.items():
+            if oid is not None and oid not in self._subscribers:
+                oids_to_remove.append(oid)
+                continue
+            for destination, subscription_info in destinations.items():
+                self._send_msg(topic, oid, destination, msg)
+                if subscription_info.get('only_once', False):
+                    self._unsubscribe(topic, oid, destination)
+        for oid in oids_to_remove:
+            self._subscriptions[topic].pop(oid)
         
-    def _send_msg(self, topic, destination, msg):
+    def _send_msg(self, topic, oid, destination, msg):
+        if oid is not None and oid in self._subscribers:
+            destination = getattr(self._subscribers[oid], destination)
         destination(topic, msg)
 
 
--- src/epubsub/test/hub.py
+++ src/epubsub/test/hub.py
@@ -7,7 +7,13 @@ Created on 06/06/2013
 import unittest
 from .. import hub 
 
+def f(topic, msg):
+    Test.result = msg['a'] + msg['b']
+
 class Test(unittest.TestCase):
+    
+    result = 0
+    
     class A(object):
         def __init__(self):
             self.result = 0
@@ -15,24 +21,37 @@ class Test(unittest.TestCase):
             self.result = msg['a'] + msg['b']
         def sumAcum(self, topic, msg):
             self.result += msg['a']
+        def sumABExt(self, topic, msg):
+            self.__class__.result = msg['a'] + msg['b']
 
     def setUp(self):
         self.a = self.A()
+        Test.result = 0
         self.bus = hub.Hub()
+
+    def tearDown(self):
+        self.bus.close("topicA")
+        self.bus.close("topicB")
+        self.bus.close("topicC")
 
     def testSimpleTopic(self):
         self.bus.subscribe("topicA", self.a.sumAB)
+        self.bus.subscribe("topicA", f)
+
         self.bus.publish("topicA", {'a':2,'b':5})
         self.assertEqual(self.a.result, 7)
+        self.assertEqual(Test.result, 7)
     
     def testMultiSubscribers(self):
         self.a2 = self.A()
         self.bus.subscribe("topicA", self.a.sumAB)
         self.bus.subscribe("topicA", self.a2.sumAB)
+        self.bus.subscribe("topicA", f)
         self.bus.publish("topicA", {'a':2,'b':5})
         self.assertEqual(self.a.result, 7)
         self.assertEqual(self.a2.result, 7)
-
+        self.assertEqual(Test.result, 7)
+        
     def testLostMsgs(self):
         self.a2 = self.A()
         self.bus.subscribe("topicA", self.a.sumAcum)
@@ -53,12 +72,17 @@ class Test(unittest.TestCase):
 
     def testUnsubscribe(self):
         self.bus.subscribe("topicA", self.a.sumAB)
+        self.bus.subscribe("topicA", f)
         self.bus.unsubscribe("topicA", self.a.sumAB)
+        self.bus.unsubscribe("topicA", f)
         self.bus.publish("topicA", {'a':2,'b':5})
         self.assertEqual(self.a.result, 0)
+        self.assertEqual(Test.result, 0)
         
         with self.assertRaises(KeyError):
             self.bus.unsubscribe("topicA", self.a.sumAB)
+        with self.assertRaises(KeyError):
+            self.bus.unsubscribe("topicA", f)
         with self.assertRaises(KeyError):
             self.bus.unsubscribe("topicB", self.a.sumAB)
         with self.assertRaises(KeyError):
@@ -67,15 +91,27 @@ class Test(unittest.TestCase):
     def testClose(self):
         self.a2 = self.A()
         self.bus.subscribe("topicA", self.a.sumAB)
+        self.bus.subscribe("topicA", f)
         self.bus.subscribe("topicA", self.a2.sumAB)
 
         self.bus.close("topicA")
         self.bus.publish("topicA", {'a':2,'b':5})
         self.assertEqual(self.a.result, 0)
         self.assertEqual(self.a2.result, 0)
+        self.assertEqual(Test.result, 0)
         
         self.bus.close("topicA")
         
+    def testDelDestination(self):                        
+        self.bus.subscribe("topicA", self.a.sumABExt)
+        self.bus.publish("topicA", {'a':2,'b':5})
+        self.assertEqual(self.A.result, 7)
+        
+        self.A.result = 0
+        self.assertEqual(self.A.result, 0)
+        del self.a
+        self.bus.publish("topicA", {'a':2,'b':5})
+        self.assertEqual(self.A.result, 0)
         
         
 
