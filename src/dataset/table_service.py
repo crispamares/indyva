e847b26b2576d3735c82a98a04187f2a32aa69c4
Juan Morales <juan.morales@upm.es>
2013-11-04 19:31:27 +0100

    table_service gainned expose_table, aggregate and distinct methods

--- src/dataset/table_service.py
+++ src/dataset/table_service.py
@@ -23,6 +23,7 @@ class TableService(INamed):
     
     def register_in(self, dispatcher):
         dispatcher.add_method(self.new_table)
+        dispatcher.add_method(self.expose_table)
         dispatcher.add_method(self.del_table)
         # TableView properties
         dispatcher.add_method(partial(self._proxy_property, 'name'), 'name')
@@ -32,6 +33,8 @@ class TableService(INamed):
         dispatcher.add_method(partial(self._proxy, 'get_data'), 'get_data')
         dispatcher.add_method(partial(self._proxy, 'find'), 'find')
         dispatcher.add_method(partial(self._proxy, 'find_one'), 'find_one')
+        dispatcher.add_method(partial(self._proxy, 'distinct'), 'distinct')
+        dispatcher.add_method(partial(self._proxy, 'aggregate'), 'aggregate')
         dispatcher.add_method(partial(self._proxy, 'row_count'), 'row_count')
         dispatcher.add_method(partial(self._proxy, 'column_count'), 'column_count')
         dispatcher.add_method(partial(self._proxy, 'column_names'), 'column_names')
@@ -61,6 +64,10 @@ class TableService(INamed):
         new_table = Table(name, schema).data(data)
         self._tables[name] = new_table
         return new_table
+
+    def expose_table(self, name, table):
+        self._tables[name] = table
+        return table
 
     def del_table(self, name):
         self._tables.pop(name)
