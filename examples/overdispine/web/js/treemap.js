6718a786acb804fe3764fa3a97c8478a4e9b286a
Juan Morales del Olmo <juan.morales@upm.es>
2013-11-20 12:01:49 +0100

    [example] renamed:    d3treemap.js -> treemap.js

--- examples/overdispine/web/js/treemap.js
+++ examples/overdispine/web/js/treemap.js
@@ -1,6 +1,6 @@
 
 define(
-["when","WsRpc", "vega", "hub"]
+["when","WsRpc", "d3", "hub", "jquery"]
 ,
 function () {
     var hub = require('hub');
@@ -10,22 +10,64 @@ function () {
 	// Subscribe to dynamics
 	// Setter Data from a table
 	this.container = container;
-    };
 
-    treemapView.prototype.render = function() {
-	var self = this;
-	var container = this.container;
-	var spec = getTreemapSpec(970, 500, this.data);
-	vg.parse.spec(spec, function(chart) {
-			  self.view = chart({el:container});
-			  self.view.on("click", function(event, item) { 
-					   console.log(item.datum.data);
-					   hub.instance().publish('spine_selected', item.datum.data);
-				       })
-			      .update(); 
-		      });
-    };
+	var width = 960,
+	    height = 500;
 
+	var color = d3.scale.category20c();
+
+	var treemap = d3.layout.treemap()
+	    .size([width, height])
+	    .padding(4)
+	    .value(function(d) { return d.size; });
+
+	var svg = d3.select(container).append("svg")
+	    .style("width", width + "px")
+	    .style("height", height + "px");
+
+	var parent_layer = svg.append('g');
+	var leaf_layer = svg.append('g');
+
+	this.render = function() {
+
+	    var treemap_data = treemap.nodes(this.data);
+
+	    var leaves = leaf_layer.selectAll(".leaf")
+		.data(treemap_data.filter(function(d){return ! Boolean(d.children);}));
+	    var parents = parent_layer.selectAll(".parent")
+		.data(treemap_data.filter(function(d){return Boolean(d.children);}));
+
+	    parents.enter().append("rect")
+		.attr("class", "node parent")
+		.attr("fill", function(d) { return color(d.name);});
+
+	    leaves.enter().append("rect")
+		.attr("class", "node leaf")
+		.on("click", function(d) {
+			hub.instance().publish('spine_selected', d.name);});
+	    leaf_layer
+		.attr("opacity",0)
+	      .transition()
+	        .delay(400)
+		.attr("opacity",1);
+		
+	    parents
+	      .transition()
+		.duration(350)
+		.attr("x", function(d) { return d.x + "px"; })
+		.attr("y", function(d) { return d.y + "px"; })
+		.attr("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+		.attr("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+
+	    leaves
+		.attr("x", function(d) { return d.x + "px"; })
+		.attr("y", function(d) { return d.y + "px"; })
+		.attr("width", function(d) { return d.dx + "px"; })
+		.attr("height", function(d) { return d.dy + "px"; });
+	};
+
+    };
+    
     treemapView.prototype.update = function() {
 	if (this.view){
 	    console.log('update', this.view.width());
@@ -38,22 +80,7 @@ function () {
 
     treemapView.prototype.setData = function(data) {
 	this.data = data;
-/*	if (this.view) {
-	    var vg_data = [{
-		    "name": "tree",
-		    "values": data,
-		    "format": {"type": "treejson"},
-		    "transform": [
-			{"type": "treemap", "value": "data.size"}
-		    ]
-		}];
-		gvalues = data;
-		gdata = vg_data;
-		gview = this.view;
-	    console.log('data', this.data);
-	    this.view.data(vg.parse.data(vg_data).flow);
-	}
-*/    };
+    };
 
 
     function getTreemapSpec(width, height, data) {
