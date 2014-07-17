73855d0eff738ef871ed28b98c8c55c8a27c4640
Juan Morales <crispamares@gmail.com>
2014-07-17 11:18:20 +0200

    App added. Works also as a executable server #20 #21

---
+++ indyva/app.py
@@ -0,0 +1,84 @@
+#! /usr/bin/env python
+'''This module can be use as an executable to configure and run an
+Indyva Kernel. You can also inherit from :class:`App` to make your own
+core application.
+
+Provides a ``CLI`` interface for the configuration and also allows to
+set the configuration with a configuration file (ini syntax).
+
+For more details::
+    $ ./app.py --help
+
+'''
+from indyva.kernel import Kernel
+from indyva.facade.server import WSServer, ZMQServer
+from configuration import get_random_port, parse_args_and_config
+
+
+class App(object):
+    '''
+    This class provide an easy way of configure and run a kernel.
+
+    ::
+        app = App()         # Default configuration
+        app.run()           # This is blocking
+    '''
+    def __init__(self, config=None):
+        '''You can provide a configuration if you don't want to parse the
+        invocation args or a config file.
+
+        :config object config: Is an Namespace (object) with the
+        kernel configuration
+        '''
+        self.kernel = Kernel()
+        if config is None:
+            config = parse_args_and_config()
+        self.config_services(config)
+            
+
+    def config_services(self, config):
+        '''Usually you don't have to invoke this method, is invoked in the
+        ``__init__`` method.
+
+        This method creates and adds to the kernel que services
+        indicated in the configuration.
+
+        Also configures the ports that those servers are going to use.
+
+        :param object config: Is an Namespace (object) with the kernel configuration
+
+        '''
+        if config.zmq_server:
+            zmq_port = self._guess_port(config, config.zmq_port)
+            zmq_server = ZMQServer(port=zmq_port)
+            self.kernel.add_server(zmq_server)
+
+            print "* ZMQ Server listening on port: {0}".format(zmq_port)
+
+        if config.ws_server:
+            ws_port = self._guess_port(config, config.ws_port)
+            ws_server = WSServer(port=ws_port, web_dir=config.web_dir)
+            self.kernel.add_server(ws_server)      
+
+            print "* WebSocket Server listening on port: {0}".format(ws_port)
+            print "* Serving web from: {0}".format(config.web_dir)
+
+    def _guess_port(self, config, default=None):
+        if config.use_random_port:
+            port_range = (config.min_port, config.max_port)
+            max_tries = config.port_max_tries
+            port = get_random_port(port_range=port_range, max_tries=max_tries)
+        elif default is not None:
+            port = default
+        else:
+            raise Exception("Is impossible to get a free port")
+
+        return port    
+
+    def run(self):        
+        self.kernel.run_forever()
+    
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
---
+++ indyva/configuration.py
@@ -0,0 +1,96 @@
+import argparse
+import ConfigParser
+import socket
+import random
+
+default_options = [
+    ("zmq_server", dict(default=False, action='store_true', help="Run the ZMQ Server")),
+    ("zmq_port", dict(type=int, default=18000, help="The port number of the ZMQ server")),
+    ("ws_server", dict(default=False, action='store_true', help="Run the WebSocket Server")),
+    ("ws_port", dict(type=int, default=18081, help="The port number of the WebSocket server")),
+    ("web_dir", dict(default='web', help="The directory where the index.html is placed")),
+#    ("web_server", dict(default=False, action='store_true', help="Run the Web Server")),
+#    ("web_port", dict(type=int, default=18080, help="The port number of the Web server")),
+    ("use_random_port", dict(default=False, action='store_true', help="Let the system find free ports inside the port range")),
+    ("port_max_tries", dict(type=int, default=100, help="Maximum number of port finding attempts to make")),
+    ("min_port", dict(type=int, default=10000, help="The lower port of the range")),
+    ("max_port", dict(type=int, default=20000, help="The upper port of the range"))
+]
+
+def parse_args_and_config(options=None, description=None):
+    '''
+    This function reads the configuration from three sources. If the
+    user specify a ``config_file`` the config file is read. Then the
+    rest of argv is parsed overwriting the previous
+    configuration. 
+
+    The configuration options accepted by this function are described
+    in :data:`default_options`. You can change these options by
+    providing an options argument.
+
+    :param options: List of options like arguments are specified in argparse
+    :param str description: A description of what the program does, printed in the help prompt
+
+    :returns: The parsed arguments as an object (Namespace) 
+    '''
+    if options is None:
+        options = default_options
+
+    # Turn off help, so we print all options in response to -h
+    config_parser = argparse.ArgumentParser(add_help=False)
+    config_parser.add_argument("-c", "--config_file",
+                               help="Specify configuration file", metavar="FILE")
+    args, remaining_argv = config_parser.parse_known_args()
+    
+    defaults = { k:v['default'] for k, v in options }
+        
+    if args.config_file:
+        config = ConfigParser.SafeConfigParser()
+        config.read([args.config_file])
+        config_defaults = dict(config.items("Core"))
+        
+        defaults.update(config_defaults)
+            
+    # Don't surpress add_help here so it will handle -h
+    parser = argparse.ArgumentParser(
+        description=description,
+        # Inherit options from config_parser
+        parents=[config_parser],
+        # Print default values in the help message
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument('--version', action='version', version='PROGRAM VERSION')
+    for option in options:
+        parser.add_argument("--"+option[0], **option[1])
+
+    parser.set_defaults(**defaults)
+
+    args = parser.parse_args(remaining_argv)
+
+    return args
+
+
+
+def get_random_port(port_range, max_tries):
+    '''
+    Return a free port in a range
+    
+    :param port_range: (int,int) The min and max port numbers
+    :param max_tries: The maximum number of bind attempts to make
+    :return port: int, the port ready to use
+    '''
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    for i in range(max_tries):
+        try:
+            port = random.randrange(port_range[0], port_range[1])
+            s.bind(('127.0.0.1',port))
+        except socket.error as e:
+            if not e.errno == socket.errno.EADDRINUSE:
+                raise
+        else:
+            s.close()
+            return port
+    raise Exception("Could not find a free random port.")
+    
+
