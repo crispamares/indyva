f900a0531ba84dc007c65ad24dbec367320ddc02
Juan Morales del Olmo <juan.morales@upm.es>
2014-09-04 20:56:34 +0200

    Adds grava Root. Just the first idea

--- indyva/grava.py
+++ indyva/grava.py
@@ -1,24 +1,46 @@
 '''
 Created on 22/01/2014
 
-@author: crispamares
+:author: Juan Morales
 '''
+from indyva.names import INamed
+
 
 class IDefined(object):
     '''
-    An IDefined has a grammar that represents its configuration and state 
+    An IDefined has a grammar that represents its configuration and state
     '''
 
     @property
     def grammar(self):
         raise NotImplementedError('The grammar property has to be implemented')
-    
-    
-if __name__ == '__main__':
-    class Defined(IDefined):
-        @property
-        def grammar(self):
-            return {1:1}
-            
-    d = Defined()
-    print Defined.grammar.fget(d)
+
+    @staticmethod
+    def build(cls, grammar):
+        raise NotImplementedError('The build static method has to be implemented')
+
+
+class Root(IDefined, INamed):
+
+    def __init__(self, name, prefix=''):
+        INamed.__init__(self, name, prefix=prefix)
+
+        self._builders = {}
+        self._nodes = {}
+
+    def register(self, key, builder):
+        self._builders[key] = builder
+
+    def add_node(self, name, node):
+        self._nodes[name] = node
+
+    @property
+    def grammar(self):
+        gv = {}
+        for name, node in self._nodes.items:
+            gv[name] = node.grammar
+        return gv
+
+    @staticmethod
+    def build(cls, grammar):
+        pass
