47c6270b8d7e4f0ffb72df9d8014fb13702be5f5
Juan Morales del Olmo <juan.morales@upm.es>
2014-09-04 13:19:24 +0200

    Adds SharedObjectService

---
+++ indyva/dataset/shared_object_service.py
@@ -0,0 +1,90 @@
+'''
+Created on Jul 19, 2013
+
+:author: Juan Morales
+'''
+
+from functools import partial
+
+from indyva.names import INamed
+from indyva.facade.showcase import Case
+from indyva.dataset.shared_object import SharedObject, VersionError
+
+
+class SharedObjectService(INamed):
+    '''
+    This class provide a facade for managing SharedObjects
+    '''
+
+    def __init__(self, name='SharedObjectSrv'):
+        '''
+        @param name: The unique name of the service
+        '''
+        self._shared_objects = Case()
+        INamed.__init__(self, name)
+
+    def register_in(self, dispatcher):
+        dispatcher.add_method(self.new_shared_object)
+        dispatcher.add_method(self.expose_shared_object)
+        dispatcher.add_method(self.del_shared_object)
+        dispatcher.add_method(self.push)
+        # SharedObject properties
+        dispatcher.add_method(partial(self._proxy_property, 'name'), 'name')
+        dispatcher.add_method(partial(self._proxy_property, 'grammar'), 'grammar')
+        # SharedObject methods
+        dispatcher.add_method(partial(self._proxy, 'pull'), 'pull')
+
+    def _proxy(self, method, shared_object_oid, *args, **kwargs):
+        shared_object = self._shared_objects[shared_object_oid]
+        result = shared_object.__getattribute__(method)(*args, **kwargs)
+        return result
+
+    def _proxy_property(self, method, shared_object_oid):
+        shared_object = self._shared_objects[shared_object_oid]
+        result = shared_object.__getattribute__(method)
+        return result
+
+    def push(self, oid, data, version):
+        '''
+        :param oid: The SharedObject oid
+        :param data: list or dict. The updated data.
+        :param version: This should be the returned by pull or push methods.
+
+        :returns: tuple(conflict_ocurred, new_version)
+                 conflict_ocurred: True if provided version is older than current
+                                   False otherwise
+                 new_version: The version generated after the modification.
+                              Becomes the current one.
+        '''
+        shared_object = self._shared_objects[oid]
+        conflict = False
+        try:
+            new_version = shared_object.push(data, version)
+        except VersionError:
+            conflict = True
+            new_version = None
+
+        return (conflict, new_version)
+
+    def new_shared_object(self, name, data, prefix=''):
+        '''
+        :param str name: If a name is not provided, an uuid is generated
+        :param data: A dict or list
+        :param str prefix: Prepended to the name creates the oid
+        '''
+        new_shared_object = SharedObject(name, data, prefix=prefix)
+        self._shared_objects[new_shared_object.oid] = new_shared_object
+        return new_shared_object
+
+    def expose_shared_object(self, shared_object):
+        self._shared_objects[shared_object.oid] = shared_object
+        return shared_object
+
+    def del_shared_object(self, oid):
+        self._shared_objects.pop(oid)
+
+    def __getattr__(self, method):
+        if method in ['name', 'grammar']:
+            return partial(self._proxy_property, method)
+        else:
+            return partial(self._proxy, method)
--- indyva/facade/front.py
+++ indyva/facade/front.py
@@ -7,7 +7,7 @@ Created on 17/07/2013
 
 from indyva.external.tinyrpc.dispatch import RPCDispatcher
 from .front_service import FrontService
-from indyva.dataset import table_service
+from indyva.dataset import table_service, shared_object_service
 from indyva.epubsub import hub_service
 from indyva.dynamics import condition_service, dselect_service, dfilter_service
 from indyva.IO import io_service
@@ -19,11 +19,11 @@ class Front(RPCDispatcher):
     '''
     @staticmethod
     def instance():
-        """Returns a global `Loop` instance. 
-        
+        """Returns a global `Loop` instance.
+
         :warning: Not ThreadSafe.
         """
-        if not hasattr(Front, "_instance"):        
+        if not hasattr(Front, "_instance"):
             Front._instance = Front()
         return Front._instance
 
@@ -31,7 +31,7 @@ class Front(RPCDispatcher):
     def initialized():
         """Returns true if the singleton instance has been created."""
         return hasattr(Front, "_instance")
-        
+
     def install(self):
         """Installs this `Front` object as the singleton instance.
 
@@ -41,23 +41,24 @@ class Front(RPCDispatcher):
         """
         assert not Front.initialized()
         Front._instance = self
-        
+
     def __init__(self):
         RPCDispatcher.__init__(self)
-        
+
         self.add_method(self.echo)
         self.add_service(FrontService(self, 'FrontSrv'))
         self.add_service(table_service.TableService('TableSrv'))
+        self.add_service(shared_object_service.SharedObjectService('SharedObjectSrv'))
         self.add_service(hub_service.HubService('HubSrv'))
         self.add_service(condition_service.ConditionService('ConditionSrv'))
         self.add_service(dselect_service.DynSelectService('DynSelectSrv', 'ConditionSrv'))
         self.add_service(dfilter_service.DynFilterService('DynFilterSrv', 'ConditionSrv'))
         self.add_service(io_service.IOService('IOSrv', 'TableSrv'))
-        
+
     def add_service(self, service):
         subdispatcher = RPCDispatcher()
         service.register_in(subdispatcher)
-        self.add_subdispatch(subdispatcher, service.name+'.')
-        
+        self.add_subdispatch(subdispatcher, service.name + '.')
+
     def echo(self, a):
         return a
